0515abe8b8df8a4b926a5c7774392b6683fd361a
blade-01 <animashauntaofiq@gmail.com>
2022-02-22 02:22:57 +0100

    Chore: Update contents on Business Analytics \page

--- src/store/modules/buss.js
+++ src/store/modules/buss.js
@@ -1,145 +1,127 @@
+import axios from 'axios'
+
+const baseUrl = 'https://www.api.re.technifyincubator.com/api/v1'
+
 const state = {
   recommendations: [
     {
       id: Math.random().toString(36).substring(7).toUpperCase(),
-      src: require("@/assets/img/blog-01.png"),
+      src: require('@/assets/img/blog-01.png'),
       title:
         "The European Union's competition regulator has given the all-clear to Microsoft's $19.7BN purchase of transcription tech firm Nuance",
-      btn: "Read",
+      btn: 'Read'
     },
     {
       id: Math.random().toString(36).substring(7).toUpperCase(),
-      src: require("@/assets/img/blog-02.png"),
+      src: require('@/assets/img/blog-02.png'),
       title:
-        "Dogecoin spikes more than 20% after Elon Musk says Tesla will accept it as payment for merch",
-      btn: "Read",
+        'Dogecoin spikes more than 20% after Elon Musk says Tesla will accept it as payment for merch',
+      btn: 'Read'
     },
     {
       id: Math.random().toString(36).substring(7).toUpperCase(),
-      src: require("@/assets/img/blog-03.png"),
+      src: require('@/assets/img/blog-03.png'),
       title:
-        "A Bluetooth bug in a popular at-home COVID-19 test could falsify results",
-      btn: "Read",
+        'A Bluetooth bug in a popular at-home COVID-19 test could falsify results',
+      btn: 'Read'
     },
     {
       id: Math.random().toString(36).substring(7).toUpperCase(),
-      src: require("@/assets/img/blog-01.png"),
+      src: require('@/assets/img/blog-01.png'),
       title:
         "The European Union's competition regulator has given the all-clear to Microsoft's $19.7BN purchase of transcription tech firm Nuance",
-      btn: "Read",
+      btn: 'Read'
     },
     {
       id: Math.random().toString(36).substring(7).toUpperCase(),
-      src: require("@/assets/img/blog-02.png"),
+      src: require('@/assets/img/blog-02.png'),
       title:
-        "Dogecoin spikes more than 20% after Elon Musk says Tesla will accept it as payment for merch",
-      btn: "Read",
+        'Dogecoin spikes more than 20% after Elon Musk says Tesla will accept it as payment for merch',
+      btn: 'Read'
     },
     {
       id: Math.random().toString(36).substring(7).toUpperCase(),
-      src: require("@/assets/img/blog-03.png"),
+      src: require('@/assets/img/blog-03.png'),
       title:
-        "A Bluetooth bug in a popular at-home COVID-19 test could falsify results",
-      btn: "Read",
-    },
+        'A Bluetooth bug in a popular at-home COVID-19 test could falsify results',
+      btn: 'Read'
+    }
   ],
-  vids: [
-    {
-      id: Math.random().toString(36).substring(7).toUpperCase(),
-      title:
-        "The European Union's competition regulator has given the all-clear to Microsoft's $19.7BN purchase of transcription tech firm Nuance",
-      src: "https://www.youtube.com/embed/N7CUoJ-cdEE",
-    },
-    {
-      id: Math.random().toString(36).substring(7).toUpperCase(),
-      title:
-        "The European Union's competition regulator has given the all-clear to Microsoft's $19.7BN purchase of transcription tech firm Nuance",
-      src: "https://www.youtube.com/embed/gElfIo6uw4g",
-    },
-    {
-      id: Math.random().toString(36).substring(7).toUpperCase(),
-      title:
-        "The European Union's competition regulator has given the all-clear to Microsoft's $19.7BN purchase of transcription tech firm Nuance",
-      src: "https://www.youtube.com/embed/cLnFSqpyQDg",
-    },
-    {
-      id: Math.random().toString(36).substring(7).toUpperCase(),
-      title:
-        "The European Union's competition regulator has given the all-clear to Microsoft's $19.7BN purchase of transcription tech firm Nuance",
-      src: "https://www.youtube.com/embed/gElfIo6uw4g",
-    },
-    {
-      id: Math.random().toString(36).substring(7).toUpperCase(),
-      title:
-        "The European Union's competition regulator has given the all-clear to Microsoft's $19.7BN purchase of transcription tech firm Nuance",
-      src: "https://www.youtube.com/embed/cLnFSqpyQDg",
-    },
-    {
-      id: Math.random().toString(36).substring(7).toUpperCase(),
-      title:
-        "The European Union's competition regulator has given the all-clear to Microsoft's $19.7BN purchase of transcription tech firm Nuance",
-      src: "https://www.youtube.com/embed/N7CUoJ-cdEE",
-    },
-  ],
+  videos: [],
   reports: [
     {
       id: Math.random().toString(36).substring(7).toUpperCase(),
-      src: require("@/assets/img/blog-01.png"),
+      src: require('@/assets/img/blog-01.png'),
       title:
         "The European Union's competition regulator has given the all-clear to Microsoft's $19.7BN purchase of transcription tech firm Nuance",
-      btn: "Read",
+      btn: 'Read'
     },
     {
       id: Math.random().toString(36).substring(7).toUpperCase(),
-      src: require("@/assets/img/blog-02.png"),
+      src: require('@/assets/img/blog-02.png'),
       title:
-        "Dogecoin spikes more than 20% after Elon Musk says Tesla will accept it as payment for merch",
-      btn: "Read",
+        'Dogecoin spikes more than 20% after Elon Musk says Tesla will accept it as payment for merch',
+      btn: 'Read'
     },
     {
       id: Math.random().toString(36).substring(7).toUpperCase(),
-      src: require("@/assets/img/blog-03.png"),
+      src: require('@/assets/img/blog-03.png'),
       title:
-        "A Bluetooth bug in a popular at-home COVID-19 test could falsify results",
-      btn: "Read",
+        'A Bluetooth bug in a popular at-home COVID-19 test could falsify results',
+      btn: 'Read'
     },
     {
       id: Math.random().toString(36).substring(7).toUpperCase(),
-      src: require("@/assets/img/blog-01.png"),
+      src: require('@/assets/img/blog-01.png'),
       title:
         "The European Union's competition regulator has given the all-clear to Microsoft's $19.7BN purchase of transcription tech firm Nuance",
-      btn: "Read",
+      btn: 'Read'
     },
     {
       id: Math.random().toString(36).substring(7).toUpperCase(),
-      src: require("@/assets/img/blog-02.png"),
+      src: require('@/assets/img/blog-02.png'),
       title:
-        "Dogecoin spikes more than 20% after Elon Musk says Tesla will accept it as payment for merch",
-      btn: "Read",
+        'Dogecoin spikes more than 20% after Elon Musk says Tesla will accept it as payment for merch',
+      btn: 'Read'
     },
     {
       id: Math.random().toString(36).substring(7).toUpperCase(),
-      src: require("@/assets/img/blog-03.png"),
+      src: require('@/assets/img/blog-03.png'),
       title:
-        "A Bluetooth bug in a popular at-home COVID-19 test could falsify results",
-      btn: "Read",
-    },
-  ],
-};
+        'A Bluetooth bug in a popular at-home COVID-19 test could falsify results',
+      btn: 'Read'
+    }
+  ]
+}
 
 const getters = {
   recommendations: (state) => state.recommendations,
-  vids: (state) => state.vids,
-  reports: (state) => state.reports,
-};
+  videos: (state) => state.videos,
+  reports: (state) => state.reports
+}
 
-const actions = {};
+const actions = {
+  getVideos({ commit }) {
+    axios.get(`${baseUrl}/rec/?size=18`).then((response) => {
+      commit('getVideos', response.data.data)
+    })
+  },
+  getSinglevideo({ commit }, id) {
+    axios.get(`${baseUrl}/video/${id}`).then((response) => {
+      commit('getSingleVideo', response.data.data)
+    })
+  }
+}
 
-const mutations = {};
+const mutations = {
+  getVideos(state, payload) {
+    state.videos = payload
+  }
+}
 
 export default {
   state,
   actions,
   getters,
-  mutations,
-};
+  mutations
+}
--- src/views/solutions/BusinessView.vue
+++ src/views/solutions/BusinessView.vue
@@ -4,26 +4,10 @@
     <div class="container">
       <div class="bus-content routes">
         <h3>Bussiness Analytic Insight</h3>
-        <div class="banner">
-          <ul>
-            <li class="banner-main-icon">
-              <img src="@/assets/img/bus-folder.svg" alt="icon" />
-            </li>
-            <div class="banner-inner">
-              <li v-for="ban in bans" :key="ban">
-                <img :src="ban.icon" alt="icon" />
-                <div class="ban-aside">
-                  <p>{{ ban.title }}</p>
-                  <p>{{ ban.content }}</p>
-                </div>
-              </li>
-            </div>
-          </ul>
-        </div>
       </div>
     </div>
   </div>
-  <div class="buss">
+  <!-- <div class="buss">
     <div class="container">
       <div class="buss-header">
         <h3>Top Recommendation</h3>
@@ -46,7 +30,7 @@
         <button class="see-more">See more</button>
       </div>
     </div>
-  </div>
+  </div> -->
   <div class="buss">
     <div class="container">
       <div class="buss-header">
@@ -54,9 +38,9 @@
         <button class="hide-on-sm see-more">See more</button>
       </div>
       <div class="buss-content">
-        <div v-for="vid in vids" :key="vid" class="buss-bg">
+        <div v-for="vid in videos" :key="vid" class="buss-bg">
           <iframe
-            :src="vid.src"
+            :src="formatLink(vid.link)"
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -72,7 +56,7 @@
       </div>
     </div>
   </div>
-  <div class="buss">
+  <!-- <div class="buss">
     <div class="container">
       <div class="buss-header">
         <h3>Trending News</h3>
@@ -91,57 +75,68 @@
         <button class="see-more">See more</button>
       </div>
     </div>
-  </div>
+  </div> -->
   <Footer />
 </template>
 
 <script>
-import Header from "@/components/navbar/Header.vue";
-import Footer from "@/components/reuseables/Footer.vue";
-import { mapGetters } from "vuex";
+import Header from '@/components/navbar/Header.vue'
+import Footer from '@/components/reuseables/Footer.vue'
+import { mapActions, mapGetters } from 'vuex'
 export default {
   components: {
     Header,
-    Footer,
+    Footer
   },
   data() {
     return {
       bans: [
         {
           id: Math.random().toString(36).substring(7).toUpperCase(),
-          icon: require("@/assets/img/bus-user.svg"),
-          title: "Name",
-          content: "Kiyatsu Nagasaki",
+          icon: require('@/assets/img/bus-user.svg'),
+          title: 'Name',
+          content: 'Kiyatsu Nagasaki'
         },
         {
           id: Math.random().toString(36).substring(7).toUpperCase(),
-          icon: require("@/assets/img/bus-company.svg"),
-          title: "Company",
-          content: "Technify",
+          icon: require('@/assets/img/bus-company.svg'),
+          title: 'Company',
+          content: 'Technify'
         },
         {
           id: Math.random().toString(36).substring(7).toUpperCase(),
-          icon: require("@/assets/img/bus-role.svg"),
-          title: "Role",
-          content: "Product Designer",
+          icon: require('@/assets/img/bus-role.svg'),
+          title: 'Role',
+          content: 'Product Designer'
         },
         {
           id: Math.random().toString(36).substring(7).toUpperCase(),
-          icon: require("@/assets/img/bus-location.svg"),
-          title: "Location",
-          content: "Lagos, Nigeria",
-        },
-      ],
-    };
+          icon: require('@/assets/img/bus-location.svg'),
+          title: 'Location',
+          content: 'Lagos, Nigeria'
+        }
+      ]
+    }
   },
   computed: {
-    ...mapGetters(["recommendations", "vids", "reports"]),
+    ...mapGetters(['recommendations', 'videos'])
   },
-};
+  methods: {
+    ...mapActions(['getVideos']),
+    formatLink(value) {
+      return (
+        value.substr(0, 24) + 'embed' + '/' + value.substr(32, value.length)
+      )
+    }
+  },
+  mounted() {
+    this.getVideos()
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-@import "@/scss/global.scss";
+@import '@/scss/global.scss';
 @mixin seeMore() {
   background: none;
   border: none;
@@ -172,43 +167,6 @@ export default {
       color: $header;
       margin-bottom: 2rem;
       text-transform: capitalize;
-    }
-    .banner {
-      background: #b53cff;
-      border: 1.5px solid #be53ff;
-      box-shadow: 0px 0px 14px 5px rgba(0, 0, 0, 0.1);
-      border-radius: 5px;
-      color: $white;
-      padding: 2.5rem 1rem;
-      margin-bottom: 2rem;
-      & ul {
-        list-style: none;
-      }
-      &-main-icon {
-        padding-bottom: 4rem;
-        & img {
-          width: 45px;
-        }
-      }
-      &-inner {
-        & li {
-          padding-top: 3rem;
-          & p {
-            font-size: 16px;
-            font-weight: 400;
-            line-height: 21.76px;
-            margin-bottom: 0.5rem;
-            &:last-child {
-              font-size: 18px;
-              font-weight: 600;
-              line-height: 24.48px;
-            }
-          }
-          & img {
-            width: 22px;
-          }
-        }
-      }
     }
   }
 }
@@ -277,16 +235,6 @@ export default {
       & h3 {
         text-align: left;
       }
-      .banner {
-        &-main-icon {
-          padding-bottom: 2rem;
-        }
-        &-inner {
-          display: flex;
-          align-items: end;
-          justify-content: space-between;
-        }
-      }
     }
   }
   .buss {
@@ -310,28 +258,6 @@ export default {
       & h3 {
         font-size: 30px;
         line-height: 41px;
-      }
-      .banner {
-        padding: 2.5rem 2rem;
-        &-inner {
-          & li {
-            display: flex;
-            align-items: flex-start;
-            text-align: left;
-            & p {
-              font-size: 18px;
-              line-height: 20px;
-              &:last-child {
-                font-size: 20px;
-                font-weight: 400;
-                line-height: 22px;
-              }
-            }
-            & img {
-              margin-right: 1rem;
-            }
-          }
-        }
       }
     }
   }
@@ -374,20 +300,6 @@ export default {
       & h3 {
         font-size: 38px;
         line-height: 51px;
-      }
-      .banner {
-        &-inner {
-          & li {
-            & p {
-              font-size: 20px;
-              line-height: 27px;
-              &:last-child {
-                font-size: 24px;
-                line-height: 32px;
-              }
-            }
-          }
-        }
       }
     }
   }
