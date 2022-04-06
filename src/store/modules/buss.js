4e0ad7d5eede821819f8d03ba5aca1dc8efea65e
blade-01 <animashauntaofiq@gmail.com>
2022-04-06 06:37:51 +0100

    Update: Requested only youtube video links

--- src/store/modules/buss.js
+++ src/store/modules/buss.js
@@ -1,127 +1,49 @@
-import axios from 'axios'
+import axios from "axios";
 
-const baseUrl = 'https://www.api.re.technifyincubator.com/api/v1'
+const baseUrl = "https://www.api.re.technifyincubator.com/api/v1";
 
 const state = {
-  recommendations: [
-    {
-      id: Math.random().toString(36).substring(7).toUpperCase(),
-      src: require('@/assets/img/blog-01.png'),
-      title:
-        "The European Union's competition regulator has given the all-clear to Microsoft's $19.7BN purchase of transcription tech firm Nuance",
-      btn: 'Read'
-    },
-    {
-      id: Math.random().toString(36).substring(7).toUpperCase(),
-      src: require('@/assets/img/blog-02.png'),
-      title:
-        'Dogecoin spikes more than 20% after Elon Musk says Tesla will accept it as payment for merch',
-      btn: 'Read'
-    },
-    {
-      id: Math.random().toString(36).substring(7).toUpperCase(),
-      src: require('@/assets/img/blog-03.png'),
-      title:
-        'A Bluetooth bug in a popular at-home COVID-19 test could falsify results',
-      btn: 'Read'
-    },
-    {
-      id: Math.random().toString(36).substring(7).toUpperCase(),
-      src: require('@/assets/img/blog-01.png'),
-      title:
-        "The European Union's competition regulator has given the all-clear to Microsoft's $19.7BN purchase of transcription tech firm Nuance",
-      btn: 'Read'
-    },
-    {
-      id: Math.random().toString(36).substring(7).toUpperCase(),
-      src: require('@/assets/img/blog-02.png'),
-      title:
-        'Dogecoin spikes more than 20% after Elon Musk says Tesla will accept it as payment for merch',
-      btn: 'Read'
-    },
-    {
-      id: Math.random().toString(36).substring(7).toUpperCase(),
-      src: require('@/assets/img/blog-03.png'),
-      title:
-        'A Bluetooth bug in a popular at-home COVID-19 test could falsify results',
-      btn: 'Read'
-    }
-  ],
+  articles: [],
+  news: [],
   videos: [],
-  reports: [
-    {
-      id: Math.random().toString(36).substring(7).toUpperCase(),
-      src: require('@/assets/img/blog-01.png'),
-      title:
-        "The European Union's competition regulator has given the all-clear to Microsoft's $19.7BN purchase of transcription tech firm Nuance",
-      btn: 'Read'
-    },
-    {
-      id: Math.random().toString(36).substring(7).toUpperCase(),
-      src: require('@/assets/img/blog-02.png'),
-      title:
-        'Dogecoin spikes more than 20% after Elon Musk says Tesla will accept it as payment for merch',
-      btn: 'Read'
-    },
-    {
-      id: Math.random().toString(36).substring(7).toUpperCase(),
-      src: require('@/assets/img/blog-03.png'),
-      title:
-        'A Bluetooth bug in a popular at-home COVID-19 test could falsify results',
-      btn: 'Read'
-    },
-    {
-      id: Math.random().toString(36).substring(7).toUpperCase(),
-      src: require('@/assets/img/blog-01.png'),
-      title:
-        "The European Union's competition regulator has given the all-clear to Microsoft's $19.7BN purchase of transcription tech firm Nuance",
-      btn: 'Read'
-    },
-    {
-      id: Math.random().toString(36).substring(7).toUpperCase(),
-      src: require('@/assets/img/blog-02.png'),
-      title:
-        'Dogecoin spikes more than 20% after Elon Musk says Tesla will accept it as payment for merch',
-      btn: 'Read'
-    },
-    {
-      id: Math.random().toString(36).substring(7).toUpperCase(),
-      src: require('@/assets/img/blog-03.png'),
-      title:
-        'A Bluetooth bug in a popular at-home COVID-19 test could falsify results',
-      btn: 'Read'
-    }
-  ]
-}
+  reports: [],
+};
 
 const getters = {
-  recommendations: (state) => state.recommendations,
+  articles: (state) => state.articles,
+  news: (state) => state.news,
   videos: (state) => state.videos,
-  reports: (state) => state.reports
-}
+  reports: (state) => state.reports,
+};
 
 const actions = {
   getVideos({ commit }) {
-    axios.get(`${baseUrl}/rec/?size=18`).then((response) => {
-      commit('getVideos', response.data.data)
-    })
+    axios.get(`${baseUrl}/rec`).then((response) => {
+      const responses = response.data.data;
+      const articles = responses
+        .filter((res) => res.type === "ARTICLE")
+        .slice(0, 6);
+      const videos = responses
+        .filter((res) => res.source === "Youtube")
+        .slice(0, 6);
+      const news = responses.filter((res) => res.type === "NEWS").slice(0, 6);
+      state.videos = videos;
+      state.articles = articles;
+      state.news = news;
+      commit("getVideos", videos);
+    });
   },
-  getSinglevideo({ commit }, id) {
-    axios.get(`${baseUrl}/video/${id}`).then((response) => {
-      commit('getSingleVideo', response.data.data)
-    })
-  }
-}
+};
 
 const mutations = {
   getVideos(state, payload) {
-    state.videos = payload
-  }
-}
+    state.videos = payload;
+  },
+};
 
 export default {
   state,
   actions,
   getters,
-  mutations
-}
+  mutations,
+};
